f31c3721cdddc91918ba597ef04f2b1e93b70e44
Andreas Jonsson <andreas.jonsson@kreablo.se>
2019-05-01 23:38:45 +0200

    Borrower-completion backend/UI adjustments

---
+++ Koha/Completion/AddressServiceBorrowerCompletion.pm
@@ -0,0 +1,126 @@
+package Koha::Completion::AddressServiceBorrowerCompletion;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+use strict;
+use IO::Socket::SSL;
+use SOAP::Lite;
+use DateTime;
+use Koha::Logger;
+
+require Exporter;
+our @ISA = qw( Exporter );
+our @EXPORT = qw(fetch_completions);
+our @EXPORT_OK = qw(normalize_pnr);
+
+
+sub normalize_pnr {
+    my $pnr = shift;
+
+    if ($pnr =~ /^(\d{2}|\d{4})(\d{2})(\d{2})(?:[-+]?)(\d{4})$/) {
+	my $y = int($1);
+	my $m = int($2);
+	my $d = int($3);
+	my $n = int($4);
+
+	if ($y < 100) {
+	    my $cy = DateTime->now->year % 100;
+	    if ($y < $cy) {
+		$y += 2000;
+	    } else {
+		$y += 1900;
+	    }
+	}
+
+	return ("$y$m$d-$n", "$y$m$d$n", "$y-$m-$d");
+    }
+    return undef;
+}
+
+sub fetch_completions {
+    my $id = shift;
+    my $logger = Koha::Logger->get({ category => 'Koha.Completion.AddressServiceBorrowerCompletion'});
+
+    my ($pnrd, $pnrn, $dob) = normalize_pnr($id);
+
+    if (defined $pnrn) {
+
+	my $soap = SOAP::Lite->proxy("https://adresservice.ltv.se/csp/population/LTV.AddressService.Service.GetAddressResponderBinding.cls", ssl_opts => {
+	    SSL_cert_file => "/etc/ssl/certs/kovast-addressservice.cert.pem",
+	    SSL_key_file => "/etc/ssl/private/kovast-addressservice.key2.pem",
+	    SSL_ca_file => "/etc/ssl/certs/SITHS-cacerts.pem",
+	    SSL_use_cert => 1
+        });
+
+	$soap->on_action( sub { 'ltv:population:resident:AddressService:GetAddress' } );
+	$soap->ns( 'ltv:population:resident:AddressService' );
+
+	my $resp = $soap->call('GetAddress', SOAP::Data->name('pnr')->value($pnrn));
+
+	if ($resp->fault) {
+	    my $msg = $resp->faultcode . ' ' . $resp->faultstring;
+	    $logger->error($msg);
+	    return { error => $msg, status => 500 };
+	}
+
+	my @form_fields = ();
+
+	push @form_fields, {
+	    'name' => 'patron_attr_',
+	    'attrname' => 'PERSNUMMER',
+	    'value' => $pnrd
+	};
+	push @form_fields, {
+	    'name' => 'dateofbirth',
+	    'value' => $dob	
+	};
+
+	my %map = (
+	    'ENamn' => 'surname',
+	    'FNamn' => 'firstname',
+	    'PostAdress' => 'city',
+	    'GatuAdress' => 'address',
+	    'PostNr' => 'address2'
+        );
+	
+	while (my ($srcname, $targetname) = each %map) {
+	    if (defined($resp->result->{$srcname})) {
+		push @form_fields, {
+		    'name' => $targetname,
+		    'value' => $resp->result->{$srcname}
+		}
+	    }
+	}
+
+	return { form_fields => \@form_fields };
+	
+    } else {
+	my $msg = "Felaktigt personnummer: " . $id;
+	$logger->error($msg);
+	return { error => $msg, status => 400 };
+	
+    }
+}
+
+
+=head1 AUTHOR
+
+Andreas Jonsson, E<lt>andreas.jonsson@kreablo.seE<gt>
+
+=cut
+
+1;
--- koha-tmpl/intranet-tmpl/prog/js/form-completer.js
+++ koha-tmpl/intranet-tmpl/prog/js/form-completer.js
@@ -7,7 +7,8 @@
     "input": "input",
     "button": "button.btn-primary",
     "fetch_msg": null,
-    "modal_form_id": null
+    "modal_form_id": null,
+    "id_field": null
   };
 
   function FormCompleter(opts) {
@@ -38,10 +39,33 @@
     var $modal = $(this.opts.modal);
     var $input = $modal.find(this.opts.input);
     var $button = $modal.find(this.opts.button);
+
+    var form_id = '';
+    if (Array.isArray(this.opts.form_id)) {
+      for (var i = 0; i < this.opts.form_id.length; i+=1) {
+	var fi = this.opts.form_id[i];
+	form_id += (i > 0 ? ', #' : '#') + fi;
+      }
+    } else {
+      form_id = '#' + this.opts.form_id;
+    }
+    this.form_id = form_id;
+    console.log("form id: " + form_id);
+
     var self = this;
     var $modal_form = this.opts.modal_form_id !== null ? $("#" + this.opts.modal_form_id) : $modal.parents("form");
     $modal.on("shown.bs.modal", function () {
       $input.focus();
+      if (self.opts.id_field !== null) {
+	var $id_field = self.input_for(self.opts.id_field);
+	$id_field.each(function (index, element) {
+	  var val = $(element).val();
+	  if (typeof val !== "undefined" && val != "") {
+	    $input.val(val);
+	    return false;
+	  }
+	})
+      }
     });
     var submit = function submit (event) {
       event.stopPropagation();
@@ -80,32 +104,81 @@
 
   p.done = function done (data, textStatus, jqXHR) {
     $(this.opts.modal).modal('hide');
-    console.log("form id: " + this.opts.form_id)
-    var $form = $('#' + this.opts.form_id);
-    for (var i = 0; i < data.form_fields.length; i++) {
+    for (var i = 0; i < data.form_fields.length; i+=1) {
       var f = data.form_fields[i];
       console.log('[name="' + f.name + '"]');
-      $form.find('[name="' + f.name + '"]').val(f.value);
+      
+      var $input = this.input_for(f);
+
+      $input.each(function (index, element) {
+	var old = $(element).val();
+	if (old != f.value) {
+	  $(element).val(f.value);
+	  $(element).addClass('form-completer-updated');
+	}
+      })
     }
   };
+
+  p.input_for =  function inputs_for (f) {
+    var $form = $(this.form_id);
+    if (typeof f.attrname !== "undefined") {
+      var inputs = [];
+      $form.each(function (index, form) {
+	var names = [];
+	$(form).find('[value="' + f.attrname + '"]').each(function (index, element) {
+	  console.log('element.name ' + element.name);
+	  if (element.name.startsWith(f.name) && element.name.endsWith('_code')) {
+	    var name = element.name.substring(0, element.name.length - '_code'.length);
+	    if (names.indexOf(name) < 0) {
+	      names.push(name);
+	    }
+	  }
+	});
+	console.log(JSON.stringify(names));
+	for (var i = 0; i < names.length; i+=1) {
+	  var name = names[i];
+	  console.log("setting " + '[name="' + name + '"]' + " to " + f.value);
+	  var $inputs = $(form).find('[name="' + name + '"]');
+	  for (var j = 0;  j < $inputs.length; j+=1) {
+	    inputs.push($inputs.get(j));
+	  }
+	}
+      });
+      return $(inputs);
+    } else {
+      return $form.find('[name="' + f.name + '"]');
+    }
+  }
+
+  var displayError = function (msg, jqXHR, textStatus, errorThrown) {
+    var msg = "<h3>" + msg + "</h3>";
+    if (typeof jqXHR.responseJSON.error !== undefined) {
+      msg += "<p>" + jqXHR.responseJSON.error + "</p>";
+    }
+    humanMsg.displayMsg( msg, { className: "humanError"} );
+  }
 
   p.fail = function fail (jqXHR, textStatus, errorThrown) {
   }
 
   p.handle400 = function handle400 (jqXHR, textStatus, errorThrown) {
-    
+    displayError(MSG_BAD_REQUEST, jqXHR, textStatus, errorThrown);
   }
   p.handle401 = function handle401 (jqXHR, textStatus, errorThrown) {
-    
+    displayError(MSG_UNAUTHORIZED, jqXHR, textStatus, errorThrown);    
+  }
+  p.handle403 = function handle403 (jqXHR, textStatus, errorThrown) {
+    displayError(MSG_FORBIDDEN, jqXHR, textStatus, errorThrown);    
   }
   p.handle404 = function handle404 (jqXHR, textStatus, errorThrown) {
-    
+    displayError(MSG_NOT_FOUND, jqXHR, textStatus, errorThrown);
   }
   p.handle500 = function handle500 (jqXHR, textStatus, errorThrown) {
-    
+    displayError(MSG_INTERNAL_SERVER_ERROR, jqXHR, textStatus, errorThrown);
   }
   p.handle503 = function handle503 (jqXHR, textStatus, errorThrown) {
-    
+    displayError(MSG_SERVICE_UNAVAILABLE, jqXHR, textStatus, errorThrown);
   }
   
   p.always = function always () {
