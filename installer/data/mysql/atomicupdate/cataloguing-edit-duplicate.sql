363fce4716032f50fdc0bc52fe7ed485b0f06086
Andreas Jonsson <andreas.jonsson@kreablo.se>
2021-02-16 16:03:18 +0100

    Generate new controlfields 001 and 003 on edit duplicate record

---
+++ installer/data/mysql/atomicupdate/cataloguing-edit-duplicate.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO systempreferences (`variable`, `value`, `explanation`, `options`, `type`)
+VALUES
+('DuplicateBiblioSet003', '', 'If set, editing a biblio record as a new record will set control field 003 to this value and generate a locally unique 001', NULL, 'Free');
+
+
