e3e1e2d6d145579b4b40ea486c4a4dfa5ad9f0b3
Andreas Jonsson <andreas.jonsson@kreablo.se>
2021-02-16 10:40:26 +0100

    Grouped libraries in opac and intranet catalogue listings and holdings pickup library selector

--- Koha/Template/Plugin/Branches.pm
+++ Koha/Template/Plugin/Branches.pm
@@ -94,6 +94,9 @@ sub all {
     return $libraries;
 }
 
+
+use Data::Dumper;
+
 sub all_grouped {
     my ( $self, $params ) = @_;
     my $selected = $params->{selected};
@@ -134,12 +137,16 @@ sub all_grouped {
               unless ref($item) eq 'Koha::Item';
             @libraries = @{ $item->pickup_locations( { patron => $patron } ) }
               if defined $item;
+
+
+            warn "item: " . Dumper(\@libraries);
         }
         elsif ($biblio) {
             $biblio = Koha::Biblios->find($biblio)
               unless ref($biblio) eq 'Koha::Biblio';
             @libraries = @{ $biblio->pickup_locations( { patron => $patron } ) }
               if defined $biblio;
+            warn "biblio: " . Dumper(\@libraries);
         }
 
         map { push @branchcodes, $_->branchcode } @libraries;
@@ -165,6 +172,7 @@ sub all_grouped {
     }
     
     while (my ($key, $value)  = each %{$params->{search_params}}) {
+        next if $key eq 'patron' or $key eq 'biblio';
         if ($where eq '') {
             $where .= ' WHERE ';
         } else {
