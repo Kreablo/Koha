f9f5340d49a1a5fe3c0f75883ee98462fb5a7a46
Andreas Jonsson <andreas.jonsson@kreablo.se>
2021-03-10 11:32:03 +0100

    Fix authorize on patron attribute for 20.05

--- C4/SIP/Sip/MsgType.pm
+++ C4/SIP/Sip/MsgType.pm
@@ -1017,9 +1017,12 @@ sub handle_patron_info {
     $resp = (PATRON_INFO_RESP);
     if (defined $patron) {
         if (defined $server->{account}->{authorize_on_patron_attribute}) {
-            my $attr = C4::Members::Attributes::GetBorrowerAttributeValue($patron->{borrowernumber}, $server->{account}->{authorize_on_patron_attribute});
+            my $kp = Koha::Patrons->find( {
+                borrowernumber => $patron->{borrowernumber}
+            } );
+            my $attr = $kp->get_extended_attribute($server->{account}->{authorize_on_patron_attribute});
 	    my $val = defined $server->{account}->{authorize_on_patron_attribute_value} ? $server->{account}->{authorize_on_patron_attribute_value} : '1';
-            unless (defined $attr && $attr eq $val) {
+            unless (defined $attr && $attr->attribute eq $val) {
                 $patron = undef;
             }
         }
