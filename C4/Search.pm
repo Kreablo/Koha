e586499cac9617e9890ef9380b04c824d3fcf5e5
Nick Clemens <nick@bywatersolutions.com>
2021-08-25 15:26:23 +0000

    Bug 28371: Passpreviously fetched branches and itemtypes through and fetch all needed AV at once
    This patch updates the searchResuls code to pass through the pre-constructed branches and itemtype lookups
    to XSLTParse4Display to avoid repeating this
    
    It also updates getAuthorisedValues4MARCSubfields to fetch the values for mapped subfields and pass
    then through to transforMarc4XSLT
    
    Note that we currently blank invalid branches and itemtypes - I presrve this, we should open another bug
    if we want to change this behaviour
    
    Changes are covered by tests
    
    To test:
    1 - Perform searches in OPAC and staff client that return many records
    2 - Use the 'Network' tab on the browser console (opened with F12 usually) to see the time taken
    3 - Note the speed before the patch
    4 - Apply patch
    5 - restart all the things
    6 - Note improvement in speed
    
    **Note: The improvement is more drastic the more items per record, try adding large numbers of items to your search results to test**
    
    Signed-off-by: Andreas Jonsson <andreas.jonsson@kreablo.se>

--- C4/Search.pm
+++ C4/Search.pm
@@ -1982,7 +1982,9 @@ sub searchResults {
                     ),
                     fix_amps       => 1,
                     hidden_items   => \@hiddenitems,
-                    xslt_variables => $xslt_variables
+                    xslt_variables => $xslt_variables,
+                    branches => \%branches,
+                    itemtypes => \%itemtypes
                 }
             );
         }
