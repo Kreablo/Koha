910c651c963619c93c18d1681570d0faa1af0368
Katrin Fischer <katrin.fischer.83@web.de>
2022-08-08 21:18:19 +0000

    Bug 30200: (follow-up) Update database update and pref description
    * Rephrases the system preference description a bit
    * Adds IGNORE to the database statement
    * Update output message of database statement to include pref name
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/atomicupdate/Bug_30200-Add_ILLRequestsTabs.pl
+++ installer/data/mysql/atomicupdate/Bug_30200-Add_ILLRequestsTabs.pl
@@ -2,13 +2,13 @@ use Modern::Perl;
 
 return {
     bug_number => "30200",
-    description => "Add customizable tabs to interlibrary loan requests list",
+    description => "Add ILLRequestsTabs system preference",
     up => sub {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
         # Do you stuffs here
         $dbh->do(q{
-            INSERT INTO `systempreferences`
+            INSERT IGNORE INTO `systempreferences`
                 (variable,value,options,explanation,type)
             VALUES
                 ('ILLRequestsTabs','','','Add customizable tabs to interlibrary loan requests list','Textarea');
