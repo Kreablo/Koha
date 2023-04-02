799bea88465fe3d8ead5fe93a37044d4311170a6
Andreas Jonsson <andreas.jonsson@kreablo.se>
2023-04-02 10:24:24 +0200

    SMS support for platformPartnerId mapping

--- C4/Letters.pm
+++ C4/Letters.pm
@@ -1560,6 +1560,7 @@ sub _send_message_by_sms {
         {
             destination => $patron->smsalertnumber,
             message     => $message->{'content'},
+	    patron      => $patron,
         }
     );
 
